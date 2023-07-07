5b60837d7504d47177c5a9da8499c1a5659fef71
MKzing <mkzing33@gmail.com>
2023-07-07 15:05:02 -0600

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,33 +1,116 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
 
-    function printAge() {
-        const output = `${firstName}, You are ${age}, born in ${birthYear}`;
-        console.log(output);
+//     function printAge() {
+//         const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//         console.log(output);
 
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            // New variable, same name
-            const firstName = "Steven";
-            // Reassigning variable
-            const output = "Echoooooo";
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true;
+//             // New variable, same name
+//             const firstName = "Steven";
+//             // Reassigning variable
+//             const output = "Echoooooo";
 
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
+//             const str = `Oh, and you're a millenial, ${firstName}`;
+//             console.log(str);
 
-            function add(a,b) {
-                return a + b;
-            }
-        }
-        console.log(millenial);
-        // console.log add(2,3);
-    }
-    printAge();
-    return age;
-}
+//             function add(a,b) {
+//                 return a + b;
+//             }
+//         }
+//         console.log(millenial);
+//         // console.log add(2,3);
+//     }
+//     printAge();
+//     return age;
+// }
 
 
-const firstName = 'Jonas';
-calcAge(1991);
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// // Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// // console.log(addArrow(2,3));
+// console.log(addArrow);
+
+
+// function addDecl(a,b) {
+//     return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+
+// if(!numProducts) {
+//     deleteShoppingCart();
+// }
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//     console.log('All products deleted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+
+// calcAgeArrow(1980);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function() {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     },
+// };
+
+// jonas.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
