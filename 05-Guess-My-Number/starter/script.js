7254678b891fa0299a28b7f9b1f85662de8f39da
MKzing <mkzing33@gmail.com>
2023-07-07 10:12:57 -0600

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,18 +14,22 @@ let secretNumber = (Math.trunc(Math.random()*20) + 1);
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
     // 0 or no number
     if(!guess) {
-        document.querySelector('.message').textContent = "No number!";
+        displayMessage("No number!");
     }
 
     //Guess is correct!
     else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = "Woo hoo! You got it!";
+        displayMessage("Woo hoo! You got it!");
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.number').style.width = '30rem';
@@ -36,37 +40,21 @@ document.querySelector('.check').addEventListener('click', function() {
         }
 
     }
-    
-    // Guess too high
-    else if (guess > secretNumber) {
+
+    // Guess is wrong
+    else if (guess !== secretNumber) {
         if (score > 1) {
 
-            document.querySelector('.message').textContent = "Too high!";
+            displayMessage (guess > secretNumber ? "Too high!" : "Too low!");
             score--;
             document.querySelector('.score').textContent = score;
         }
         else {
-            document.querySelector('.message').textContent = "GAME OVER";
+            displayMessage("GAME OVER");
             score = 0;
             document.querySelector('.score').textContent = score;
         }    
     }
-
-    // Guess too low
-    else if (guess < secretNumber) {
-        if (score > 1) {
-
-            document.querySelector('.message').textContent = "Too low!";
-            score--;
-            document.querySelector('.score').textContent = score;
-        }
-        else {
-            document.querySelector('.message').textContent = "GAME OVER";
-            score = 0;
-            document.querySelector('.score').textContent = score;
-        }    
-    }
-
 
 });
 
