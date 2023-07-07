026b6acb720e3f4ed01ec42d8e999e426f3d88d6
MKzing <mkzing33@gmail.com>
2023-07-07 14:05:27 -0600

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            // New variable, same name
+            const firstName = "Steven";
+            // Reassigning variable
+            const output = "Echoooooo";
+
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a,b) {
+                return a + b;
+            }
+        }
+        console.log(millenial);
+        // console.log add(2,3);
+    }
+    printAge();
+    return age;
+}
+
+
+const firstName = 'Jonas';
+calcAge(1991);
