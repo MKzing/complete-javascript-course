d38d2462d001a15c845df7a4f8a3cdb7cc826559
MKzing <mkzing33@gmail.com>
2023-07-11 16:58:41 -0600

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -55,32 +55,50 @@
 
 // ** Callback Functions
 
-const oneWord = function (str) {
-    return str.replace(/ /g, ''). toLowerCase();
-};
+// const oneWord = function (str) {
+//     return str.replace(/ /g, ''). toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-    const [first, ...others] = str.split(' ');
-    return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// };
 
-//Higher-order
-const transformer = function (str, fn) {
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${fn(str)}`);
+// //Higher-order
+// const transformer = function (str, fn) {
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${fn(str)}`);
 
-    console.log(`Transformed by: ${fn.name}`);
-};
+//     console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
 
-const high5 = function() {
-    console.log('A hand!');
-};
+// const high5 = function() {
+//     console.log('A hand!');
+// };
 
-document.body.addEventListener('click', high5);
-['John', 'Mary', 'Joesephina'].forEach(high5);
+// document.body.addEventListener('click', high5);
+// ['John', 'Mary', 'Joesephina'].forEach(high5);
+
+// ** Functions to Functions
+
+// const greet = function (greeting) {
+//     return function (name) {
+//         console.log(`${greeting} ${name}`);
+//     };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Toby');
+// greeterHey('Laquanda');
+
+// greet('Hello')('Jonjon');
+
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+// greetArr('Hi')('Tommyboy');
 
 // Coding Challenge #1
 
